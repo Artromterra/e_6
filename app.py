4ac0b9daecccf2b64bac0fde99d0f34fef215413
Roman <54408191+Artromterra@users.noreply.github.com>
2020-03-30 11:32:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,18 +27,6 @@ def memo(n):
 		redis_client.set(k, result)
 	return "Число фибо: {}".format(result)
 
-# @app.route("/<number>", methods=['GET'])
-# def get_fibonacci_api(number):
-# 	number = int(number)
-# 	stored_value = redis_client.get(number)
-# 	if stored_value:
-# 		logger.info("For %s stored value is used" % number)
-# 		return jsonify({number: stored_value.decode()}), 200
-# 	new_value = get_fibonacci(number)
-# 	logger.info("For %s new value is calculated" % number)
-# 	redis_client.set(number, new_value)
-# 	return jsonify({number: new_value}), 200
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port)
 
